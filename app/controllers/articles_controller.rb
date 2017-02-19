a96de328a7f48931058345f45d7791ad4304af80
Yama-Souichirou <souichirou19960210@gmail.com>
2017-02-19 11:58:44 +0000

    add googlefont all

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(set_params)
-		@article = current_user.id
+		@article.user_id = current_user.id
 		@article.save
 		redirect_to articles_path
 	end
