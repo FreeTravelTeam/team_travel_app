fdf1721e9b572f96c561283b0087cad95cc3c15c
hitomishan <baseball33hi@icloud.com>
2017-03-12 12:16:29 +0000

    migrateができた！！笑

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310062407) do
+ActiveRecord::Schema.define(version: 20170312091430) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "body"
@@ -23,10 +23,9 @@ ActiveRecord::Schema.define(version: 20170310062407) do
     t.text     "safety_info"
     t.text     "travel_item"
     t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "likes_count"
-    t.integer  "like_user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "title"
   end
 
   create_table "likes", force: :cascade do |t|
