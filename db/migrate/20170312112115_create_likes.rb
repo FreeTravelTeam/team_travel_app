51d653d138586ca7bf65d69cb40b76a41aea380f
Yama-Souichirou <souichirou19960210@gmail.com>
2017-03-12 11:26:43 +0000

    migration file repare

--- db/migrate/20170312112115_create_likes.rb
+++ db/migrate/20170312112115_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[5.0]
   def change
     create_table :likes do |t|
-      t.integer :user_id
       t.integer :article_id
+      t.integer :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310062407) do
+ActiveRecord::Schema.define(version: 20170312112408) do
 
   create_table "articles", force: :cascade do |t|
     t.text     "body"
@@ -23,16 +23,14 @@ ActiveRecord::Schema.define(version: 20170310062407) do
     t.text     "safety_info"
     t.text     "travel_item"
     t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "title"
-    t.integer  "likes_count"
-    t.integer  "like_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer  "user_id"
     t.integer  "article_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
