0a021f6540b7e26299738f80b8cbcf6323bf6f1d
hitomishan <baseball33hi@icloud.com>
2017-03-16 07:23:56 +0000

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
  def show
   @user = User.find(params[:id])
   @artcles = @user.articles
-  # userモデルからとってきたidを＠userに入れてそのidと関連付けたarticleのインスタンスをもってきてあげて@articleに入れる
- end
+  end
  
 end
