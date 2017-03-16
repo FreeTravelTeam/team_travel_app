e3aab377f672110cab39892ebb5f09e9cd68b4d7
Hitomi <baseball33hi@icloud.com>
2017-03-16 15:56:49 +0900

    ずっとpushできない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
  def show
   @user = User.find(params[:id])
   @artcles = @user.articles
+  # userモデルからとってきたidを＠userに入れてそのidと関連付けたarticleのインスタンスをもってきてあげて@articleに入れる
  end
  
 end
