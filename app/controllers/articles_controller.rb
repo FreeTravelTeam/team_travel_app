eab049c8bacfbd30e14eec80a88a91278e040eeb
Yama-Souichirou <souichirou19960210@gmail.com>
2017-03-24 17:43:53 +0000

    edit showgit branch

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
-    
     before_action :set_article, only: [:destroy, :show, :edit, :update]
+    before_action :authenticate_user!, only:[:new, :edit]
 
 	def top
 	end
